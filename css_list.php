b6872531e5b8f93a6722c64d89bbbbdcd7d3e3b2
aklump <sourcecode@intheloftstudios.com>
2016-12-08 11:18:09 -0800

    bugfix when passing absolute path.

--- css_list.php
+++ css_list.php
@@ -7,84 +7,99 @@
  *
  * @defgroup css_list CSS List
  */
-use \AKlump\CssList\CssList;
-use \AKlump\LoftDataGrids\ExportData;
+use AKlump\CssList\CssList;
+use AKlump\LoftDataGrids\ExportData;
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 try {
-  $source = array();
-  if (!empty($argv[1])) {
-    if ($argv[1] === '.' || empty($argv[1])) {
-      $source = getcwd();
-    }
-    elseif (substr($argv[1], 0, 1) === '/') {
-      $source = $argv;
-    }
-    else {
-      $source = getcwd() . '/' . $argv[1];
-    }
+    $source = array();
+    if (!empty($argv[1])) {
 
-    if (is_file($source)) {
-      $source = array($source);
-    }
-    elseif (is_dir($source)) {
-      $source = scandir($source);
-    }
-  }
+        //
+        //
+        // First figure out the source directory
+        //
+        if ($argv[1] === '.' || empty($argv[1])) {
+            $input = getcwd();
+        }
 
-  // Reduce to .css files only
-  foreach ($source as $key => $file) {
-    if (substr($file, -4) !== '.css') {
-      unset($source[$key]);
-    }
-  }
+        // Do we have an absolute path?
+        elseif (substr($argv[1], 0, 1) === '/') {
+            $input = $argv[1];
+        }
+        else {
+            $input = getcwd() . '/' . $argv[1];
+        }
 
-  if (empty($source)) {
-    throw new \InvalidArgumentException("The first argument must be a .css file or folder with at least one .css file.");
-  }
-
-  $results = array();
-  $data = new ExportData;
-  foreach ($source as $file) {
-    $contents = file_get_contents($file);
-    list($ids) = CssList::getIds($contents);
-
-    $data->setPage('Ids');
-    foreach ($ids as $id) {
-      $data->add('id', $id)->next();
+        //
+        //
+        // Now get all files in the source dir.
+        //
+        if (is_file($input)) {
+            $source = array($input);
+        }
+        elseif (is_dir($input)) {
+            $source = scandir($input);
+            array_walk($source, function (&$value) use ($input) {
+                if (substr($value, -4) !== '.css') {
+                    $value = '';
+                }
+                else {
+                    $value = $input . "/$value";
+                }
+            });
+        }
     }
 
-    list($classes, $compoundClasses, $smaCss) = CssList::getClasses($contents);
+    // Reduce to .css files only
+    $source = array_filter($source);
 
-    $classes = array_merge($classes, $compoundClasses);
-    CssList::sortClasses($classes);
-
-    $data->setPage('Classes');
-    foreach ($classes as $class) {
-      $data->add('class', $class)->next();
+    if (empty($source)) {
+        throw new \InvalidArgumentException("The first argument must be a .css file or folder with at least one .css file.");
     }
 
-    // $data->setPage('Compound Classes');
-    // foreach ($compoundClasses as $class) {
-    //   $data->add('class', $class)->next();
-    // }
+    $results = array();
+    $data = new ExportData;
+    foreach ($source as $file) {
+        $contents = file_get_contents($file);
+        list($ids) = CssList::getIds($contents);
 
-    // $data->setPage('SmaCSS Categories');
-    // foreach ($smaCss as $category => $classList) {
-    //   // foreach ($classList as $classes) {
-    //     $data->add('category', $category)
-    //     ->add('classes', implode(', ', $classList))
-    //     ->next();
-    //   // }
-    // }
-  }
+        $data->setPage('Ids');
+        foreach ($ids as $id) {
+            $data->add('id', $id)->next();
+        }
+
+        list($classes, $compoundClasses, $smaCss) = CssList::getClasses($contents);
+
+        $classes = array_merge($classes, $compoundClasses);
+        CssList::sortClasses($classes);
+
+        $data->setPage('Classes');
+        foreach ($classes as $class) {
+            $data->add('class', $class)->next();
+        }
+
+        // $data->setPage('Compound Classes');
+        // foreach ($compoundClasses as $class) {
+        //   $data->add('class', $class)->next();
+        // }
+
+        // $data->setPage('SmaCSS Categories');
+        // foreach ($smaCss as $category => $classList) {
+        //   // foreach ($classList as $classes) {
+        //     $data->add('category', $category)
+        //     ->add('classes', implode(', ', $classList))
+        //     ->next();
+        //   // }
+        // }
+    }
 
 } catch (Exception $e) {
-  print $e->getMessage() . PHP_EOL;
+    print $e->getMessage() . PHP_EOL;
 }
 
 if (isset($data)) {
-  $exporter = new \AKlump\LoftDataGrids\ValuesOnlyExporter($data);
-  print $exporter->export();
+    $exporter = new \AKlump\LoftDataGrids\ValuesOnlyExporter($data);
+    print $exporter->export();
 }
--- src/AKlump/CssList/CssList.php
+++ src/AKlump/CssList/CssList.php
@@ -13,210 +13,221 @@ namespace AKlump\CssList;
  *
  * @brief A utility class to analyze css files.
  */
-class CssList {
+class CssList
+{
 
-  public static function getIds($content) {
-    preg_match_all('/\#[a-z][^\s,:]*/im', $content, $matches);
-    $ids = $matches[0];
-    static::reduceAndSortFiles($ids);
-    static::filterOutFiles($ids);
-    static::filterOutHex($ids);
+    public static function getIds($content)
+    {
+        preg_match_all('/\#[a-z][^\s,:]*/im', $content, $matches);
+        $ids = $matches[0];
+        static::reduceAndSortFiles($ids);
+        static::filterOutFiles($ids);
+        static::filterOutHex($ids);
 
-    return array($ids);
-  }
+        return array($ids);
+    }
 
-  public static function reduceAndSortFiles(&$array) {
-    $array = array_unique($array);
-    static::sortClasses($array);
-  }
+    public static function reduceAndSortFiles(&$array)
+    {
+        $array = array_unique($array);
+        static::sortClasses($array);
+    }
 
-  public static function sortClasses(&$array) {
-    uasort($array, function ($a, $b) {
-      if ($a === $b) {
-        return 0;
-      }
+    public static function sortClasses(&$array)
+    {
+        uasort($array, function ($a, $b) {
+            if ($a === $b) {
+                return 0;
+            }
 
-      $cl = new CssList;
+            $cl = new CssList;
 
-      $aSplit = $cl->splitCompoundClasses($a);
-      $bSplit = $cl->splitCompoundClasses($b);
+            $aSplit = $cl->splitCompoundClasses($a);
+            $bSplit = $cl->splitCompoundClasses($b);
 
-      $aCount = count($aSplit);
-      $bCount = count($bSplit);
+            $aCount = count($aSplit);
+            $bCount = count($bSplit);
 
-      $aParts = $cl->parseClassName($aSplit[0]);
-      $bParts = $cl->parseClassName($bSplit[0]);
+            $aParts = $cl->parseClassName($aSplit[0]);
+            $bParts = $cl->parseClassName($bSplit[0]);
 
-      $aHas = count(array_filter($aSplit)) > 1;
-      $bHas = count(array_filter($bSplit)) > 1;
+            $aHas = count(array_filter($aSplit)) > 1;
+            $bHas = count(array_filter($bSplit)) > 1;
 
-      // If base is not equal...
-      if ($aParts[0] !== $bParts[0]) {
-        return $aParts[0] < $bParts[0] ? -1 : 1;
-      }
+            // If base is not equal...
+            if ($aParts[0] !== $bParts[0]) {
+                return $aParts[0] < $bParts[0] ? -1 : 1;
+            }
 
-      // At this point the bases are equal.
+            // At this point the bases are equal.
 
-      if ($aHas || $bHas) {
-        if ($aHas xor $bHas) {
-          // If only one has a compound then we send that
-          return $bHas ? -1 : 1;
+            if ($aHas || $bHas) {
+                if ($aHas xor $bHas) {
+                    // If only one has a compound then we send that
+                    return $bHas ? -1 : 1;
+                }
+
+                if ($aCount === $bCount) {
+                    return $a < $b ? -1 : 1;
+                }
+
+                return $aCount - $bCount;
+
+            }
+
+            // Compare on subcomponents
+            if ($aParts[2] !== $bParts[2]) {
+                return $aParts[2] < $bParts[2] ? -1 : 1;
+            }
+
+            // Compare if compound or not; compounds go bottom
+            if ($aHas xor $bHas) {
+                return $a < $b ? -1 : 1;
+            }
+        });
+
+        $array = array_values($array);
+    }
+
+    public static function splitCompoundClasses($string)
+    {
+        preg_match_all('/(\.[^\.]+)/', $string, $matches);
+
+        return count($matches[0]) > 1 ? $matches[0] : array($string);
+    }
+
+    /**
+     * Parses a class into smacss parts.
+     *
+     * @param  string $class
+     *
+     * @return array
+     *   0: module
+     *   1: submodule if any
+     *   2: subcomponent if any
+     *
+     * @code
+     *   $class = 'panel--big__title';
+     *   list($module, $submodule, $subcomponent) = $cs::parseClassName($class);
+     * @endcode
+     */
+    public static function parseClassName($class)
+    {
+        $regex = array();
+        $regex[] = '/(.+)\-\-|(.+)__|/';
+        $regex[] = '/--(.+)__|--(.+)/';
+        $regex[] = '/__(.+)/';
+
+        $return = array(null, null, null);
+        for ($i = 0; $i < count($regex); ++$i) {
+            preg_match($regex[$i], $class, $matches);
+            array_shift($matches);
+            if (($matches = array_filter($matches)) && ($matches = reset($matches))) {
+                $return[$i] = $matches;
+            }
+            elseif ($i === 0) {
+                $return[$i] = $class;
+            }
         }
 
-        if ($aCount === $bCount) {
-          return $a < $b ? -1 : 1;
+        return $return;
+    }
+
+    public static function filterOutFiles(&$array)
+    {
+        foreach ($array as $key => $value) {
+            if (preg_match('/\);?$/', $value)) {
+                unset($array[$key]);
+            }
+            if (preg_match('/\/|\.css\.map|\.scss/', $value)) {
+                unset($array[$key]);
+            }
+        }
+    }
+
+    public static function filterOutHex(&$array)
+    {
+        foreach ($array as $key => $value) {
+            if (preg_match('/^\#(?:[a-f0-9]{3}|[a-f0-9]{6})/i', $value)) {
+                unset($array[$key]);
+            }
+        }
+    }
+
+    /**
+     * Return classes and compound classes
+     *
+     * @param  string $content The css file to analyze.
+     *
+     * @return array
+     *   0 The classes array
+     *   1 The compound classes array
+     */
+    public static function getClasses($content)
+    {
+        $classes = static::_getClasses($content);
+        static::reduceAndSortFiles($classes);
+        static::filterOutFiles($classes);
+
+        // Strip off ending . from classes
+        array_walk($classes, function ($value, $key) use (&$classes) {
+            $classes[$key] = rtrim($value, '.');
+        });
+
+        // Now review each line for compound classes and pull
+        $compoundClasses = array();
+        foreach ($classes as $value) {
+            $compound = static::splitCompoundClasses($value);
+            if (count($compound) > 1) {
+                $compoundClasses[] = $value;
+                $classes = array_merge($classes, $compound);
+            }
+        }
+        // Pull out the removes
+        $classes = array_diff($classes, $compoundClasses);
+
+        static::reduceAndSortFiles($classes);
+
+        $smacssCategories = static::splitPerSmacss($classes);
+
+        return array($classes, $compoundClasses, $smacssCategories);
+    }
+
+    public static function splitPerSmacss($classesArray)
+    {
+        $smacss = array(
+            'layout'    => array(),
+            'module'    => array(),
+            'submodule' => array(),
+            'state'     => array(),
+            'theme'     => array(),
+        );
+
+        foreach ($classesArray as $class) {
+            if (preg_match('/^\.theme\-\-/i', $class)) {
+                $smacss['theme'][] = $class;
+            }
+            elseif (preg_match('/^\.layout\-\-/i', $class)) {
+                $smacss['layout'][] = $class;
+            }
+            elseif (preg_match('/^\.is\-/i', $class)) {
+                $smacss['state'][] = $class;
+            }
+            elseif (preg_match('/^[^\-]+\_\_/i', $class)) {
+                $smacss['module'][] = $class;
+            }
+            elseif (preg_match('/^[^\-]+\-\-/i', $class)) {
+                $smacss['submodule'][] = $class;
+            }
         }
 
-        return $aCount - $bCount;
-
-      }
-
-      // Compare on subcomponents
-      if ($aParts[2] !== $bParts[2]) {
-        return $aParts[2] < $bParts[2] ? -1 : 1;
-      }
-
-      // Compare if compound or not; compounds go bottom
-      if ($aHas xor $bHas) {
-        return $a < $b ? -1 : 1;
-      }
-    });
-
-    $array = array_values($array);
-  }
-
-  public static function splitCompoundClasses($string) {
-    preg_match_all('/(\.[^\.]+)/', $string, $matches);
-
-    return count($matches[0]) > 1 ? $matches[0] : array($string);
-  }
-
-  /**
-   * Parses a class into smacss parts.
-   *
-   * @param  string $class
-   *
-   * @return array
-   *   0: module
-   *   1: submodule if any
-   *   2: subcomponent if any
-   *
-   * @code
-   *   $class = 'panel--big__title';
-   *   list($module, $submodule, $subcomponent) = $cs::parseClassName($class);
-   * @endcode
-   */
-  public static function parseClassName($class) {
-    $regex = array();
-    $regex[] = '/(.+)\-\-|(.+)__|/';
-    $regex[] = '/--(.+)__|--(.+)/';
-    $regex[] = '/__(.+)/';
-
-    $return = array(NULL, NULL, NULL);
-    for ($i = 0; $i < count($regex); ++$i) {
-      preg_match($regex[$i], $class, $matches);
-      array_shift($matches);
-      if (($matches = array_filter($matches)) && ($matches = reset($matches))) {
-        $return[$i] = $matches;
-      }
-      elseif ($i === 0) {
-        $return[$i] = $class;
-      }
+        return $smacss;
     }
 
-    return $return;
-  }
+    protected static function _getClasses($content)
+    {
+        preg_match_all('/\.[a-z][^\s,:]*/im', $content, $matches);
 
-  public static function filterOutFiles(&$array) {
-    foreach ($array as $key => $value) {
-      if (preg_match('/\);?$/', $value)) {
-        unset($array[$key]);
-      }
-      if (preg_match('/\/|\.css\.map|\.scss/', $value)) {
-        unset($array[$key]);
-      }
+        return $matches[0];
     }
-  }
-
-  public static function filterOutHex(&$array) {
-    foreach ($array as $key => $value) {
-      if (preg_match('/^\#(?:[a-f0-9]{3}|[a-f0-9]{6})/i', $value)) {
-        unset($array[$key]);
-      }
-    }
-  }
-
-  /**
-   * Return classes and compound classes
-   *
-   * @param  string $content The css file to analyze.
-   *
-   * @return array
-   *   0 The classes array
-   *   1 The compound classes array
-   */
-  public static function getClasses($content) {
-    $classes = static::_getClasses($content);
-    static::reduceAndSortFiles($classes);
-    static::filterOutFiles($classes);
-
-    // Strip off ending . from classes
-    array_walk($classes, function ($value, $key) use (&$classes) {
-      $classes[$key] = rtrim($value, '.');
-    });
-
-    // Now review each line for compound classes and pull
-    $compoundClasses = array();
-    foreach ($classes as $value) {
-      $compound = static::splitCompoundClasses($value);
-      if (count($compound) > 1) {
-        $compoundClasses[] = $value;
-        $classes = array_merge($classes, $compound);
-      }
-    }
-    // Pull out the removes
-    $classes = array_diff($classes, $compoundClasses);
-
-    static::reduceAndSortFiles($classes);
-
-    $smacssCategories = static::splitPerSmacss($classes);
-
-    return array($classes, $compoundClasses, $smacssCategories);
-  }
-
-  protected static function _getClasses($content) {
-    preg_match_all('/\.[a-z][^\s,:]*/im', $content, $matches);
-
-    return $matches[0];
-  }
-
-  public static function splitPerSmacss($classesArray) {
-    $smacss = array(
-      'layout'    => array(),
-      'module'    => array(),
-      'submodule' => array(),
-      'state'     => array(),
-      'theme'     => array(),
-    );
-
-    foreach ($classesArray as $class) {
-      if (preg_match('/^\.theme\-\-/i', $class)) {
-        $smacss['theme'][] = $class;
-      }
-      elseif (preg_match('/^\.layout\-\-/i', $class)) {
-        $smacss['layout'][] = $class;
-      }
-      elseif (preg_match('/^\.is\-/i', $class)) {
-        $smacss['state'][] = $class;
-      }
-      elseif (preg_match('/^[^\-]+\_\_/i', $class)) {
-        $smacss['module'][] = $class;
-      }
-      elseif (preg_match('/^[^\-]+\-\-/i', $class)) {
-        $smacss['submodule'][] = $class;
-      }
-    }
-
-    return $smacss;
-  }
 }
